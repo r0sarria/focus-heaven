d5788f42f4dbe6121659da806f49cec345238c04
Nursena <cobanlar4228@gmail.com>
2026-08-21 01:31:22 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./focusflip/App";
+import App from "./focusheaven/App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
